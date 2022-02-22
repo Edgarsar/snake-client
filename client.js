9a5cc08dbb3d66e527af050454e12e0d5010846f
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-22 02:41:32 +0000

    movement commands experiments

--- client.js
+++ client.js
@@ -13,17 +13,34 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-// prints a message to the screen when the connection is successfully established
+  // prints a message to the screen when the connection is successfully established
   conn.on("connect", () => {
     conn.write("Successfully connected to game server");
-    // sending a Name to the server
+    // sending a name to the server
     conn.write("Name: edo");
   });
-  
+
+  // sends the string "Move: up" as data to the server,
+  /* conn.on("connect", () => {
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 50);
+
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 100);
+
+    setInterval(() => {
+      conn.write("Move: down");
+    }, 50);
+  }); */
+
 
 
   return conn;
 };
+
 
 module.exports = {
   connect
