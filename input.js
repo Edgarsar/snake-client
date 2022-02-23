0aa7b681e841caa4299022451cc473c93c798cd2
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-23 03:14:51 +0000

    add the movment keys

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,14 +15,22 @@ const setupInput = function() {
   return stdin;
 
 };
-// checks for the ctrl + c input and terminate the game
-const handleUserInput = function(key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
+
+const handleUserInput = function(data) {
+  // checks for the ctrl + c input and terminate the game
+  if (data === '\u0003') { // \u0003 maps to ctrl+c input
     process.exit();
+  } else if (data === 'w') {
+    connection.write('Move: up');
+  } else if (data === 'a') {
+    connection.write('Move: left');
+  } else if (data === 's') {
+    connection.write('Move: down');
+  } else if (data === 'd') {
+    connection.write('Move: right');
   }
 };
 
-module.exports = {
-  setupInput
-};
+module.exports = { setupInput };
+
+
